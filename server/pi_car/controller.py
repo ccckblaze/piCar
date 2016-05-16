a69f8f16479ad311262e522cefda41512c203844
Steve Han <ccckblaze@sina.com>
2016-05-16 14:52:04 +0800

    fix destructor order

--- server/pi_car/controller.py
+++ server/pi_car/controller.py
@@ -3,10 +3,13 @@ import smbus
 import logging
 import atexit
 import time
+import thread
 from threading import Thread, Timer
 
 #Use I2C or origin GPIO Ports
-USE_I2C = True
+USE_I2C = False
+I2C_ADDR = 0x20
+I2C_DEVICE = 1
 
 GPIO_FRONT_LEFT_DIR = 7
 GPIO_FRONT_LEFT_PWM = 11
@@ -19,7 +22,6 @@ GPIO_BACK_RIGHT_PWM = 22
 GPIO_CAMARA_SERVO = 29
 
 init = False
-
 def threaded(fn):
     def wrapper(*args, **kwargs):
         Thread(target=fn, args=args, kwargs=kwargs).start()
@@ -33,7 +35,29 @@ def t_init():
         GPIO.setwarnings(True)
         if USE_I2C:
             logging.debug('Using I2C')
-            init = True
+            global i2c
+            try:
+                i2c = smbus.SMBus(I2C_DEVICE)
+            except (IOError):
+                logging.debug('I2C Device Not Found!')
+            
+            # Set PCF8575 outputs
+            try :
+                # value to send
+                temp = 0x55AA
+                i2c.write_block_data(I2C_ADDR, 0, [0xEA, 0x11])
+                #i2c.write_byte(I2C_ADDR, 0xF1)
+                #print 'PCF8575 at address 0x{0:02x} WRITE 0x{1:04x}'.format( I2C_ADDR, temp )
+                #i2c.write_byte_data( I2C_ADDR, temp & 0xff, ( temp & 0xff ) >> 8 )
+            except IOError :
+                logging.debug('PCF8575 Device not found at I2C address 0x{1:02x}'.format( I2C_ADDR ))
+                error = 1
+            else :
+                # Now read from PCF8575
+                #temp = i2c.read_word_data( I2C_ADDR, 0 )
+                #print 'PCF8575 at address 0x{0:02x} READ 0x{1:04x}'.format( I2C_ADDR, temp )
+                pass
+        init = True
 
 
 def t_cleanup():
@@ -46,7 +70,8 @@ atexit.register(t_cleanup)
 
 class Wheel(object):
         
-    PWM_FREQUENCY = 50
+    PWM_FREQUENCY = 1000
+    destroyed = False
 
     def __init__(self, direction_io, pwm_io):
         logging.debug('Wheel init: ' + str(direction_io) + ', ' + str(pwm_io))
@@ -61,7 +86,10 @@ class Wheel(object):
 
     def __del__(self):
         print('Wheel destroryed')
-        #self.pwm_ch.stop()
+
+    def destory(self):
+        self.destroyed = True
+        self.pwm_ch.stop()
                 
     def forward(self):
         GPIO.output(self.dir_io, True)
@@ -80,6 +108,34 @@ class Servo(object):
     PWM_FREQUENCY = 50
     destroyed = False
 
+    class ServoLoopThread(Thread):
+        def __init__(self, owner):
+            self.owner = owner 
+            self.thread_stop = False
+            Thread.__init__(self)
+
+        def run(self):
+            while not self.thread_stop:
+                try:
+                    for i in range(0,181,10):
+                        self.owner.pwm_ch.ChangeDutyCycle(2.5 + 10 * i / 180) # Angle Speed
+                        time.sleep(0.02)                                # 20ms
+                        #self.owner.pwm_ch.ChangeDutyCycle(0)                 # Back to zero 
+                        #time.sleep(0.2)
+                     
+                    for i in range(181,0,-10):
+                        self.owner.pwm_ch.ChangeDutyCycle(2.5 + 10 * i / 180)
+                        time.sleep(0.02)
+                        #self.owner.pwm_ch.ChangeDutyCycle(0)
+                        #time.sleep(0.2)
+                except (AttributeError, TypeError):
+                    print('Exception exit')
+
+            thread.exit_thread()
+
+        def stop(self):
+            self.thread_stop = True
+
     def __init__(self, pwm_io):
         logging.debug('Servo init: ' + str(pwm_io))
         t_init()
@@ -88,54 +144,62 @@ class Servo(object):
         GPIO.setup(self.pwm_io, GPIO.OUT)
         self.pwm_ch = GPIO.PWM(self.pwm_io, self.PWM_FREQUENCY)
         self.pwm_ch.start(0)
-                
-        self.servo_loop()
+        self.servoLoopThread = self.ServoLoopThread(self)
+        self.servoLoopThread.start()
         
     def __del__(self):
         print('Servo destroryed')
-        #self.pwm_ch.stop()
        
     def destroy(self):
         self.destroyed = True
-        while(self.looping):
-            pass
-            #print('waitting', self.looping)
+        self.servoLoopThread.stop()
+        self.servoLoopThread.join()
+        self.servoLoopThread = None
+        self.pwm_ch.stop()
 
     def setAngle(self, angle):
         self.angle = angle
 
-    @threaded
-    def servo_loop(self):
-        while(not self.destroyed):
-            self.looping = True
-
-            try:
-                for i in range(0,181,10):
-                    self.pwm_ch.ChangeDutyCycle(2.5 + 10 * i / 180) # Angle Speed
-                    time.sleep(0.02)                                # 20ms
-                    #self.pwm_ch.ChangeDutyCycle(0)                 # Back to zero 
-                    #time.sleep(0.2)
-                     
-                for i in range(181,0,-10):
-                    self.pwm_ch.ChangeDutyCycle(2.5 + 10 * i / 180)
-                    time.sleep(0.02)
-                    #self.pwm_ch.ChangeDutyCycle(0)
-                    #time.sleep(0.2)
-            except (AttributeError, TypeError):
-                pass
-                #print('Exception exit') # caused by time.sleep
-
-        self.looping = False
-
 class Car(object):
         
+    TIMEOUT = 5
+    destroyed = False
+
+    checkTimeout = False
+    commandTiming = 0
     speed = 0
     decreeseFactor = 0.2
     forwarding = False
     turningLeft = False
     turningRight = False
-    destroyed = False
-        
+
+    class DetectLoopThread(Thread):
+        def __init__(self, owner):
+            self.owner = owner 
+            self.thread_stop = False
+            Thread.__init__(self)
+
+        def run(self):
+            while not self.thread_stop:
+                try:
+                    delay = 0.2
+                    if self.owner.checkTimeout:
+                        if self.owner.commandTiming >= self.owner.TIMEOUT:
+                            self.owner.stop()
+                            self.owner.commandTiming = 0
+                            self.owner.checkTimeout = False
+                            logging.debug('Timeout')
+                        else:
+                            self.owner.commandTiming += delay
+
+                    time.sleep(delay)
+
+                except (AttributeError, TypeError):
+                    print('Exception exit')
+
+        def stop(self):
+            self.thread_stop = True
+       
     def __init__(self):
         logging.debug('Car init')
         t_init()
@@ -144,19 +208,30 @@ class Car(object):
         self.wheel_back_left = Wheel(GPIO_BACK_LEFT_DIR, GPIO_BACK_LEFT_PWM)
         self.wheel_back_right = Wheel(GPIO_BACK_RIGHT_DIR, GPIO_BACK_RIGHT_PWM)
         self.camara_servo = Servo(GPIO_CAMARA_SERVO)
+        self.detectLoopThread = self.DetectLoopThread(self)
+        self.detectLoopThread.start()
 
     def __del__(self):
-        self.destroyed = True
         print('Car destroyed')
+        self.wheel_front_left.destory()
+        self.wheel_front_right.destory()
+        self.wheel_back_left.destory()
+        self.wheel_back_right.destory()
         self.camara_servo.destroy()
+
+    def destroy(self):
+        self.destroyed = True
+        self.detectLoopThread.stop()
+        self.detectLoopThread.join()
+        self.detectLoopThread = None
 
     def timeout(self):
         try:
-            print('timeout')
+            logging.debug('timeout')
             if not self.destroyed:
                 self.stop() 
         except (AttributeError, TypeError):
-            print('Exception exit')
+            logging.debug('Exception exit')
 
     def forward(self):
         self.forwarding = True 
@@ -207,16 +282,12 @@ class Car(object):
         self.updateSpeed()
         
     def setTimeout(self):
-        global timer
-        if 'timer' in globals():
-            timer.cancel()
-            timer.join()
-            #timer = None
-        timer = Timer(5, self.timeout)
-        timer.start()
+        self.checkTimeout = True
+        self.commandTiming = 0
         
     def updateSpeed(self):
-        currentSpeed = self.speed * 0.5 if self.forwarding else self.speed 
+        #currentSpeed = self.speed * 0.5 if self.forwarding else self.speed 
+        currentSpeed = self.speed
         self.wheel_front_left.setSpeed(
             self.decreeseFactor * currentSpeed if self.turningLeft else currentSpeed)
         self.wheel_front_right.setSpeed(
--- server/pi_car/views.py
+++ server/pi_car/views.py
@@ -14,44 +14,58 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from contextlib import closing
 from pi_car import app
 from controller import Car
+import atexit
+
+def cleanup():
+    print('flask cleanup')
+    if('car' in globals()):
+        global car
+        car.destroy()
+        del car
 
 @app.before_first_request
 def initialize():
-	print('initialize')
-	global car
-	car = Car()
-	car.setSpeed(100)
+    print('flask initialize')
+    if('car' not in globals()):
+        global car
+        car = Car()
+        car.setSpeed(100)
+    atexit.register(cleanup)
 
 @app.route('/')
 def show_index():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route("/login", methods=["GET", "POST"])                                   
 def login():                                                                    
-	if request.method=="GET":                                                   
-		return "get"+request.form["user"]
-	elif request.method=="POST":                                                
-		return "post"
+    if request.method=="GET":                                                   
+            return "get"+request.form["user"]
+    elif request.method=="POST":                                                
+            return "post"
 
 @app.route('/ctl',methods=['GET','POST'])
 def ctrl_id():
-	if request.method == 'POST':
-		id=request.form['id']
-		
-		if id == 't_left':
-			car.left()
-			return "left"
-		elif id == 't_right':
-			car.right()
-			return "right"
-		elif id == 't_up':
-			car.forward()
-			return "up"
-		elif id == 't_down':
-			car.backward()
-			return "down"
-		elif id == 't_stop':
-			car.stop()
-			return "stop"
+    if request.method == 'POST':
+        id=request.form['id']
+        if id == 't_left':
+            print('left')
+            car.left()
+            return "left"
+        elif id == 't_right':
+            print('right')
+            car.right()
+            return "right"
+        elif id == 't_up':
+            print('up')
+            car.forward()
+            return "up"
+        elif id == 't_down':
+            print('down')
+            car.backward()
+            return "down"
+        elif id == 't_stop':
+            print('stop')
+            car.stop()
+            return "stop"
 
-	return redirect(url_for('show_index'))
+    return redirect(url_for('show_index'))
